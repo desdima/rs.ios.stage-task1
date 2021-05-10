9eb54b1fbd0f1efa49ad4aab277014fe7e819ff3
desdima <des.dima@gmail.com>
2021-05-10 14:35:18 +0400

    All excercises is done

--- Objc_Task1/ios.stage-task1/Exercises/Exercise-5/LeaderBoardTracker.m
+++ Objc_Task1/ios.stage-task1/Exercises/Exercise-5/LeaderBoardTracker.m
@@ -12,18 +12,46 @@
                                playerArray:(NSArray <NSNumber *> *)playerArray {
     
     
-    NSMutableArray *rank = [[NSMutableArray alloc] init];
+    //NSMutableArray *rank = [[NSMutableArray alloc] init];
     
+    NSMutableArray *rankRes = [[NSMutableArray alloc] init];
     for (int i = 0; i < playerArray.count; i++) {
-        int j = 0;
         long i_num = [playerArray[i] integerValue];
-        long j_num = [rankedArray[j] integerValue];
-        while ((j < rankedArray.count - 1) && (i_num < j_num)) {
+        NSMutableArray *rank = [rankedArray mutableCopy];
+       
+        
+        [rank insertObject:playerArray[i] atIndex:rank.count];
+        
+        NSSortDescriptor *descriptor = [[NSSortDescriptor alloc] initWithKey:@"self" ascending:NO];
+        NSArray *descriptors = [NSArray arrayWithObject: descriptor];
+        NSArray *reverseOrder = [rank sortedArrayUsingDescriptors:descriptors];
+        
+        long rankIndex = 0;
+        long j = 0;
+        long j_num = [reverseOrder[0] integerValue];
+        while (i_num != j_num) {
             j++;
-            j_num = [rankedArray[j] integerValue];
+            j_num = [reverseOrder[j] integerValue];
+            if (j_num != [reverseOrder[j - 1] integerValue]) rankIndex++;
         }
-        NSNumber *s = [NSNumber numberWithInt:j + 1];
-        [rank insertObject:s atIndex:i];
+        
+        NSNumber *s = [NSNumber numberWithLong:rankIndex + 1];
+        [rankRes insertObject:s atIndex:i];
+        
+        /*
+        while (i_num <= j_num) {
+            j_num = [rank[j] integerValue];
+            if (j != rank.count - 1) {
+                j++;
+            }
+            else {
+                i_num = j_num;
+            }
+        }
+        rankIndex = j;
+        //[rank insertObject:playerArray[i] atIndex:index];
+        NSNumber *s = [NSNumber numberWithLong:rankIndex];
+        [rankRes insertObject:s atIndex:i];*/
     }
     
     
@@ -31,6 +59,35 @@
     
     
     
+    
+    
+    
+    
+    
+    
+    /*
+    for (int i = 0; i < playerArray.count; i++) {
+        int j = 0;
+        long i_num = [playerArray[i] integerValue];
+        long j_num = [rankedArray[j] integerValue];
+        //while ((j < rankedArray.count - 1) && (i_num < j_num)) {
+          //  j++;
+            //j_num = [rankedArray[j] integerValue];
+        //}
+        
+        if ((i_num >= j_num) || (j == rankedArray.count - 1)) {
+            NSNumber *s = [NSNumber numberWithInt:j + 1];
+            [rank insertObject:s atIndex:i];
+        }
+        else {
+            j++;
+            j_num = [rankedArray[j] integerValue];
+        }
+        
+    }*/
+    
+    
+    //------------------
     
     /*int j = 0;
     [rankArr insertObject:rankedArray[j] atIndex:j];
@@ -51,7 +108,7 @@
             }
         }
     }*/
-    return rank;
+    return rankRes;
 }
 
 @end
